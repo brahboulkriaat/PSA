64831b14ef6b90864ee187ce2bf7860741231568
Brahim Boulkriaat <bboulkriaat@gmail.com>
2023-02-24 20:05:30 +0100

    Data state sealed class

---
+++ app/src/main/java/com/brahimboulkriaat/psa/util/DataState.kt
@@ -0,0 +1,8 @@
+package com.brahimboulkriaat.psa.util
+
+sealed class DataState<out R> {
+    data class Success<out T>(val data: T): DataState<T>()
+    data class Error(val e: Exception): DataState<Nothing>()
+    object Loading: DataState<Nothing>()
+
+}
