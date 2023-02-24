38bae9f0e951140298cef5bfecfa541772fb0022
Brahim Boulkriaat <bboulkriaat@gmail.com>
2023-02-24 14:33:10 +0100

    Basic cities list with FAB

--- app/src/main/java/com/brahimboulkriaat/psa/ui/MainActivity.kt
+++ app/src/main/java/com/brahimboulkriaat/psa/ui/MainActivity.kt
@@ -1,42 +1,131 @@
 package com.brahimboulkriaat.psa.ui
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val mainViewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             MaterialTheme {
-                CitiesList(cities = cities)
+                /*CitiesList(cities = cities)
+
+                ExtendedFloatingActionButton(
+                    modifier = Modifier.padding(all = 8.dp),
+                    text = { Text(text = "New City") },
+                    icon = { Icon(
+                        imageVector = Icons.Rounded.Add,
+                        contentDescription = "Navigate FAB"
+
+                    ) },
+                    expanded = mainViewModel.isFabExpanded.value,
+                    onClick = { mainViewModel.fabOnClick.value.invoke()  },
+                    // containerColor = MaterialTheme.colorScheme.secondary
+                )*/
+                MainScreen(mainViewModel)
             }
         }
     }
 }
 
+@Composable
+fun MainScreen(mainViewModel: MainViewModel) {
+
+    val context = LocalContext.current
+    val listState = rememberLazyListState()
+    val expandedFabState = remember {
+        derivedStateOf {
+            listState.firstVisibleItemIndex == 0
+        }
+    }
+
+    LaunchedEffect(
+        key1 = expandedFabState.value,
+        block = {
+            mainViewModel.isFabExpanded.value = expandedFabState.value
+        })
+
+    LaunchedEffect(
+        key1 = Unit,
+        block = {
+            mainViewModel.fabOnClick.value =
+                { Toast.makeText(context, "New City", Toast.LENGTH_SHORT).show() }
+        })
+
+    LaunchedEffect(
+        key1 = Unit,
+        block = {
+            mainViewModel.itemOnClick.value =
+                { Toast.makeText(context, "Details", Toast.LENGTH_SHORT).show() }
+        })
+
+    CitiesList(listState, mainViewModel)
+
+    NewCityFab(mainViewModel)
+}
 
 @Composable
-private fun CitiesList(cities: List<City>) {
-    LazyColumn {
+fun NewCityFab(mainViewModel: MainViewModel) {
+    ExtendedFloatingActionButton(
+        modifier = Modifier.padding(all = 8.dp),
+        text = { Text(text = "New City") },
+        icon = {
+            Icon(
+                imageVector = Icons.Rounded.Add,
+                contentDescription = "Navigate FAB"
+
+            )
+        },
+        expanded = mainViewModel.isFabExpanded.value,
+        onClick = { mainViewModel.fabOnClick.value.invoke() },
+        // containerColor = MaterialTheme.colorScheme.secondary
+    )
+}
+
+@Composable
+private fun CitiesList(state: LazyListState, mainViewModel: MainViewModel, cities: List<City> = data) {
+    LazyColumn(state = state, modifier = Modifier.fillMaxSize()) {
         items(cities) { city ->
-            CityItem(city)
+            CityItem(city, mainViewModel)
         }
     }
 }
 
 @Composable
-private fun CityItem(city: City) {
-    ElevatedCard(modifier = Modifier.padding(all = 16.dp).fillMaxWidth()) {
+private fun CityItem(city: City, mainViewModel: MainViewModel) {
+    ElevatedCard(
+        modifier = Modifier
+            .padding(all = 16.dp)
+            .fillMaxWidth()
+            .clickable { mainViewModel.itemOnClick.value.invoke() }
+    ) {
         Surface(modifier = Modifier.padding(all = 8.dp)) {
             Text(text = city.name)
         }
@@ -45,7 +134,7 @@ private fun CityItem(city: City) {
 
 private data class City(val id: Long, val name: String, val lat: Double, val lon: Double)
 
-private val cities = listOf<City>(
+private val data = listOf<City>(
     City(id = 1, name = "Paris", lat = 0.0, lon = 0.0),
     City(id = 2, name = "London", lat = 0.0, lon = 0.0),
     City(id = 3, name = "New York", lat = 0.0, lon = 0.0),
@@ -54,5 +143,10 @@ private val cities = listOf<City>(
     City(id = 6, name = "Budapest", lat = 0.0, lon = 0.0),
     City(id = 7, name = "Tunis", lat = 0.0, lon = 0.0),
     City(id = 8, name = "Tokyo", lat = 0.0, lon = 0.0),
-    City(id = 9, name = "Seoul", lat = 0.0, lon = 0.0)
+    City(id = 9, name = "Seoul", lat = 0.0, lon = 0.0),
+    City(id = 10, name = "Cairo", lat = 0.0, lon = 0.0),
+    City(id = 11, name = "Dubai", lat = 0.0, lon = 0.0),
+    City(id = 12, name = "California", lat = 0.0, lon = 0.0),
+    City(id = 13, name = "Berlin", lat = 0.0, lon = 0.0),
+    City(id = 14, name = "Istanbul", lat = 0.0, lon = 0.0)
 )
--- app/src/main/java/com/brahimboulkriaat/psa/ui/MainViewModel.kt
+++ app/src/main/java/com/brahimboulkriaat/psa/ui/MainViewModel.kt
@@ -1,8 +1,14 @@
 package com.brahimboulkriaat.psa.ui
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor() : ViewModel()
+class MainViewModel @Inject constructor() : ViewModel() {
+
+    val fabOnClick = mutableStateOf<() -> Unit>({})
+    val itemOnClick = mutableStateOf<() -> Unit>({})
+    val isFabExpanded = mutableStateOf<Boolean>(true)
+}
