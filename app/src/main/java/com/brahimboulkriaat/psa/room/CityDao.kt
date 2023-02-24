7b795a8521652bcde5b19945fdc99e6dbaadc173
Brahim Boulkriaat <bboulkriaat@gmail.com>
2023-02-24 20:10:39 +0100

    Dao crud methods

--- app/src/main/java/com/brahimboulkriaat/psa/room/CityDao.kt
+++ app/src/main/java/com/brahimboulkriaat/psa/room/CityDao.kt
@@ -1,6 +1,22 @@
 package com.brahimboulkriaat.psa.room
 
-import androidx.room.Dao
+import androidx.room.*
 
 @Dao
-interface CityDao
+interface CityDao {
+
+    @Query("SELECT * FROM ${CityEntity.ENTITY_NAME}")
+    suspend fun getAll(): List<CityEntity>
+
+    @Query("SELECT * FROM ${CityEntity.ENTITY_NAME} WHERE id=:id")
+    suspend fun getById(id: Long): CityEntity
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(vararg psa: CityEntity): List<Long>
+
+    @Update
+    suspend fun update(vararg psa: CityEntity): Int
+
+    @Delete
+    suspend fun delete(vararg psa: CityEntity): Int
+}
--- app/src/main/java/com/brahimboulkriaat/psa/room/CityEntity.kt
+++ app/src/main/java/com/brahimboulkriaat/psa/room/CityEntity.kt
@@ -4,10 +4,14 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = CityEntity.ENTITY_NAME)
 class CityEntity(
     @PrimaryKey @ColumnInfo(name = "id") val id: Long,
     @ColumnInfo(name = "name") val name: String,
     @ColumnInfo(name = "lat") val lat: Double,
     @ColumnInfo(name = "lon") val lon: Double
-)
+) {
+    companion object {
+        const val ENTITY_NAME = "cities"
+    }
+}
