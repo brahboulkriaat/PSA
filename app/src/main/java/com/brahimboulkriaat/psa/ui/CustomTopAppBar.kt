4447e264a0bb97d540749015fa7e28dc061dac82
Brahim Boulkriaat <bboulkriaat@gmail.com>
2023-02-25 12:32:13 +0100

    Custom shared TopAppBar

---
+++ app/src/main/java/com/brahimboulkriaat/psa/ui/CustomTopAppBar.kt
@@ -0,0 +1,19 @@
+package com.brahimboulkriaat.psa.ui
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomTopAppBar(navController: NavController, title: String, showBackIcon: Boolean) {
+    TopAppBar(title = { Text(text = title) }, navigationIcon = {
+        if (showBackIcon && navController.previousBackStackEntry != null) {
+            IconButton(onClick = {navController.navigateUp()}) {
+                Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Back")
+            }
+        }
+    })
+}
